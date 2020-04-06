7e9aa916da1afd213f05905bd1f89f702681520b
Shubat Max <shubat.max@gmail.com>
2020-04-06 10:00:09 +0300

    add basic logic (no requests)

--- src/components/Pages/LoginPage/index.js
+++ src/components/Pages/LoginPage/index.js
@@ -3,24 +3,22 @@ import { connect } from "react-redux";
 import Cookies from "universal-cookie";
 import axios from "axios";
 import styled from "styled-components";
-import { systemStartup, authorize } from "../../../actions";
-
-import FldEmail from "../../Forms/FormComponents/FldEmail";
-import FldPassword from "../../Forms/FormComponents/FldPassword";
-import BtnLogin from "../../Forms/FormComponents/BtnLogin";
-import CbxRememberMe from "../../Forms/FormComponents/CbxRememberMe";
-import LnkForgotPassword from "../../Forms/FormComponents/LnkForgotPassword";
-import LoginError from "../../Errors/LoginError";
-import ForgotPasswordModal from "../../Modals/ForgotPasswordModal";
 
 import { Container, Row, Col } from "react-grid";
 
+
+
+
 const LoginPage = () => {
-  React.useEffect(() => {
+  React.useLayoutEffect(() => {
     document.title = "Авторизация | Uniqum";
   });
   const [login, setLogin] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [rememberMe, setRememberMe] = React.useState(false);
+  const [credentialsMatch, setCredentialsMatch] = React.useState(false);
+  const [loginAttempted, setLoginAttempted] = React.useState(false);
+  const [matchErrorVisible] = React.useState(<MatchError>Логин и пароль не совпадают</MatchError>);
 
   return (
     <Background>
@@ -34,6 +32,12 @@ const LoginPage = () => {
           </Col>
           <Col xs={12} sm={12} md={12} lg={6} xl={6}>
             <RightSideWrapper>
+              {/*<MatchError*/}
+              {/*  visible={credentialsMatch}*/}
+              {/*>Логин и пароль не совпадают</MatchError>*/}
+
+              <MatchError visible={loginAttempted && !credentialsMatch}>Логин и пароль не совпадают</MatchError>
+              {/*{ !credentialsMatch ? null : matchErrorVisible}*/}
               <form>
                 <div>
                   <BlockInputGroup>
@@ -44,6 +48,8 @@ const LoginPage = () => {
                       placeholder="your@email.com"
                       autocomplete="off"
                       tabIndex="1"
+                      value={login}
+                      onChange={(e) => {setLogin(e.target.value)}}
                     />
                   </BlockInputGroup>
                   <BlockInputGroup>
@@ -53,6 +59,8 @@ const LoginPage = () => {
                       id="password"
                       autocomplete="off"
                       tabIndex="2"
+                      value={password}
+                      onChange={(e) => {setPassword(e.target.value)}}
                     />
                   </BlockInputGroup>
                 </div>
@@ -62,12 +70,28 @@ const LoginPage = () => {
                       type="checkbox"
                       id="remember"
                       tabIndex="3"
+                      checked={rememberMe}
+                      onChange={(e) => {
+                        setRememberMe(e.target.checked)
+                      }}
                     />
                     Запомнить меня
                   </RememberMe>
                   <ForgotPassword href="#">Забыли пароль?</ForgotPassword>
                 </ToolsGroup>
-                <LoginButton type="button" tabIndex="4">Войти</LoginButton>
+                <LoginButton
+                  type="button"
+                  tabIndex="4"
+                  onClick={() => {
+                    !loginAttempted && setLoginAttempted(!loginAttempted);
+                    if(login === 'admin' && password === 'root'){
+                      setCredentialsMatch(true);
+                    }else{
+                      setCredentialsMatch(false);
+                    }
+                  }}
+                >Войти
+                </LoginButton>
               </form>
               <PoweredBy>Powered by React</PoweredBy>
             </RightSideWrapper>
@@ -78,6 +102,17 @@ const LoginPage = () => {
   );
 };
 
+
+export const MatchError = styled.div`
+  display: ${props => (props.visible ? 'block' : 'none')};
+  padding: 10px;
+  margin: 0 0 20px;
+  border: 1px solid rgba(255, 0, 0, 0.5);
+  background: #ebebeb;
+  font: normal normal 400 10px/1em "Roboto", sans-serif;
+  text-transform: uppercase;
+  color: rgb(159, 0, 0);
+`;
 const Background = styled.div`
   position: fixed;
   top: 0;
@@ -137,7 +172,6 @@ const BlockInputGroup = styled.div`
     display: block;
   }
 `;
-
 const Input = styled.input`
   width: calc(100% - 20px);
   border: 1px solid #d2d2d2;
@@ -165,11 +199,11 @@ const RememberMe = styled.label`
   & > input[type="checkbox"] {
     margin-right: 5px;
     transform: translateY(2px);
+    cursor: pointer;
   }
 `;
 const ForgotPassword = styled.a`
   float: right;
-  //margin: 0 5px 0 0; 
   font: normal normal 300 12px/1em "Roboto", sans-serif;
   letter-spacing: 1px;
 `;
@@ -186,13 +220,14 @@ const LoginButton = styled.button`
   background: #00afef;
   color: #fff;
   cursor: pointer;
+  user-select: none;
   
   &:hover {
     background: rgba(0,175,239,0.9);
   }
 `;
 const PoweredBy = styled.div`
-padding-bottom: 10px;
+  padding-bottom: 10px;
   margin: 20px 0 0;
   text-align: center;
   font: normal normal 400 12px/1em "Roboto", sans-serif;
@@ -200,142 +235,119 @@ padding-bottom: 10px;
   user-select: none;
 `;
 
-const FormBackground = styled.div`
-  background-color: #0d47a1;
-  height: 100%;
-  width: 100%;
-`;
-
-
-
-
-
-
 export default LoginPage;
 
-export class LoginPagePrev extends Component {
-  state = {
-    loginErrorDisplay: false,
-    loginHighlight: false,
-    passwordHighlight: false,
-    showForgotPassword: false
-    // login: "",
-    // pwd: ""
-  };
-
-  // componentDidMount() {
-  //   document.title = "Uniqum | Авторизация";
-  // }
-
-  render() {
-    return (
-      <div className="login-wrapper">
-        {this.getForgotPasswordModal()}
-
-        <form action="#" className="login-form">
-          <div className="logo-placeholder">
-            <div className="logo">
-              <img
-                src="./assets/img/uniqum-logo.png"
-                alt="Логотип платформы Uniqum"
-              />
-            </div>
-          </div>
-
-          {this.getLoginError()}
-          <div className="credentials">
-            <FldEmail changeHandler={this.handlerLoginChange.bind(this)} />
-            <FldPassword changeHandler={this.handlerPwdChange.bind(this)} />
-          </div>
-          <div className="tools">
-            <CbxRememberMe />
-            <LnkForgotPassword onClick={() => this.modalFPSwitch(true)} />
-          </div>
-          <BtnLogin
-            authorize={() =>
-              this.checkIdentity(this.state.login, this.state.pwd)
-            }
-          />
-        </form>
-
-        <div className="tech-version">v0.7.5</div>
-      </div>
-    );
-  }
-
-  getLoginError = () => {
-    if (this.state.loginErrorDisplay) {
-      return <LoginError hideError={() => this.loginErrorSwitch(false)} />;
-    }
-  };
-
-  getForgotPasswordModal = () => {
-    if (this.state.showForgotPassword) {
-      return <ForgotPasswordModal onClose={() => this.modalFPSwitch(false)} />;
-    }
-  };
-
-  handlerLoginChange = login => {
-    this.setState({
-      login: login.trim()
-    });
-  };
-
-  handlerPwdChange = pwd => {
-    this.setState({
-      pwd: pwd
-    });
-  };
-
-  loginErrorSwitch = state => {
-    this.setState({
-      loginErrorDisplay: state
-    });
-  };
-
-  modalFPSwitch = state => {
-    this.setState({
-      showForgotPassword: state
-    });
-  };
-
-  checkIdentity = (login, pwd) => {
-    // axios.post(`http://localhost:3001/users/auth`, {
-    axios
-      .post(`/api/users/auth`, {
-        email: login,
-        pwd: pwd
-      })
-      .then(res => {
-        const user = res.data;
-        if (user) {
-          if (user._id && user.role) {
-            this.props.systemStartup(user._id, user.role);
-            this.props.authorize(user);
-            const cookies = new Cookies();
-            cookies.set("UID", user._id, { path: "/" });
-            cookies.set("URID", user.role, { path: "/" });
-
-            switch (user.role) {
-              case 1:
-                this.props.history.push("/admin");
-                break;
-              case 2:
-                this.props.history.push("/trainer");
-                break;
-              case 3:
-                this.props.history.push("/trainee");
-                break;
-              default:
-                this.loginErrorSwitch(true);
-                return null;
-            }
-          }
-        }
-        this.loginErrorSwitch(true);
-      });
-  };
-}
+// export class LoginPagePrev extends Component {
+//   state = {
+//     loginErrorDisplay: false,
+//     loginHighlight: false,
+//     passwordHighlight: false,
+//     showForgotPassword: false
+//     // login: "",
+//     // pwd: ""
+//   };
+//
+//   // componentDidMount() {
+//   //   document.title = "Uniqum | Авторизация";
+//   // }
+//
+//   render() {
+//     return (
+//       <div className="login-wrapper">
+//         {this.getForgotPasswordModal()}
+//
+//         <form action="#" className="login-form">
+//           <div className="logo-placeholder">
+//             <div className="logo">
+//               <img
+//                 src="./assets/img/uniqum-logo.png"
+//                 alt="Логотип платформы Uniqum"
+//               />
+//             </div>
+//           </div>
+//
+//           {this.getLoginError()}
+//           <div className="credentials">
+//             <FldEmail changeHandler={this.handlerLoginChange.bind(this)} />
+//             <FldPassword changeHandler={this.handlerPwdChange.bind(this)} />
+//           </div>
+//           <div className="tools">
+//             <CbxRememberMe />
+//             <LnkForgotPassword onClick={() => this.modalFPSwitch(true)} />
+//           </div>
+//           <BtnLogin
+//             authorize={() =>
+//               this.checkIdentity(this.state.login, this.state.pwd)
+//             }
+//           />
+//         </form>
+//
+//         <div className="tech-version">v0.7.5</div>
+//       </div>
+//     );
+//   }
+//
+//   getLoginError = () => {
+//     if (this.state.loginErrorDisplay) {
+//       return <LoginError hideError={() => this.loginErrorSwitch(false)} />;
+//     }
+//   };
+//
+//   getForgotPasswordModal = () => {
+//     if (this.state.showForgotPassword) {
+//       return <ForgotPasswordModal onClose={() => this.modalFPSwitch(false)} />;
+//     }
+//   };
+//
+//   loginErrorSwitch = state => {
+//     this.setState({
+//       loginErrorDisplay: state
+//     });
+//   };
+//
+//   modalFPSwitch = state => {
+//     this.setState({
+//       showForgotPassword: state
+//     });
+//   };
+//
+//   checkIdentity = (login, pwd) => {
+//     // axios.post(`http://localhost:3001/users/auth`, {
+//     axios
+//       .post(`/api/users/auth`, {
+//         email: login,
+//         pwd: pwd
+//       })
+//       .then(res => {
+//         const user = res.data;
+//         if (user) {
+//           if (user._id && user.role) {
+//             this.props.systemStartup(user._id, user.role);
+//             this.props.authorize(user);
+//             const cookies = new Cookies();
+//             cookies.set("UID", user._id, { path: "/" });
+//             cookies.set("URID", user.role, { path: "/" });
+//
+//             switch (user.role) {
+//               case 1:
+//                 this.props.history.push("/admin");
+//                 break;
+//               case 2:
+//                 this.props.history.push("/trainer");
+//                 break;
+//               case 3:
+//                 this.props.history.push("/trainee");
+//                 break;
+//               default:
+//                 this.loginErrorSwitch(true);
+//                 return null;
+//             }
+//           }
+//         }
+//         this.loginErrorSwitch(true);
+//       });
+//   };
+// }
 
 // export default connect(null, {
 //     systemStartup,
